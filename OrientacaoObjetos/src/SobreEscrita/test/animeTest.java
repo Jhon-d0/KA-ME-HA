25c4790813275b54605112fdce6b2e0dba91a8c2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-11 10:44:03 -0300

    Metodo final

---
+++ OrientacaoObjetos/src/SobreEscrita/test/animeTest.java
@@ -0,0 +1,13 @@
+package SobreEscrita.test;
+
+import SobreEscrita.dominio.Anime;
+
+public class animeTest {
+    public static void main(String[] args) {
+
+        Anime anime1 = new Anime("AOT");
+
+        System.out.println(anime1.toString());
+
+    }
+}
